cd041d3af9c5aa40dcc2b7bc39f586f007cdb880
Deepak Meena <deepmeena70@gmail.com>
2021-06-20 09:50:21 +0530

    mobile responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,17 +19,25 @@ const vid = () => {
 
 // portfolio
 const portfolio = () => {
-    gsap.to('.port-1', { y: 300, duration: 1.5 });
-    gsap.to('.port-5', { y: -300, duration: 1.5 });
-    gsap.to('.port-3', { y: 300, duration: 1.5 });
+    if (innerWidth > 800) {
+        gsap.to('.port-1', { y: 300, duration: 1.5 });
+        gsap.to('.port-5', { y: -300, duration: 1.5 });
+        gsap.to('.port-3', { y: 300, duration: 1.5 });
+    }
+
 };
 // contact form
 const contactForm = () => {
-    gsap.to('.contact-form-animate', { scale: 1.2, duration: 1 });
+    if (innerWidth > 500) {
+        gsap.to('.contact-form-animate', { scale: 1.2, duration: 1 });
+    }
 };
 
 const contactFormRe = () => {
-    gsap.to('.contact-form-animate', { scale: 1, duration: 1 });
+    if (innerWidth > 500) {
+        gsap.to('.contact-form-animate', { scale: 1, duration: 1 });
+    }
+
 };
 
 const animate_lvl_1 = (target, fun, className, ...el) => {
@@ -56,9 +64,17 @@ const animate_lvl_1 = (target, fun, className, ...el) => {
 
 const ready = () => {
     document.body.style.display = 'block';
-    let animationClass = document.getElementsByClassName("animation");
-    for (let el of animationClass) {
-        el.style.visibility = "hidden";
+    if (innerWidth > 800) {
+        let animationClass = document.getElementsByClassName("animation");
+        for (let el of animationClass) {
+            el.style.visibility = "hidden";
+        }
+    }
+    if (innerWidth < 500) {
+        let animationClass = document.getElementsByClassName("mb-animation");
+        for (let el of animationClass) {
+            el.style.visibility = "hidden";
+        }
     }
 };
 
@@ -115,9 +131,244 @@ const animate_lvl_2 = (target, fun, className) => {
     observer.observe(document.querySelector(target));
 };
 
+const mobileAnimate_lvl_1 = (fun, className, ...target) => {
+
+    let options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.3
+    };
+
+    const callback = (entries) => {
+
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting && document.querySelector(target).classList.contains(className)) {
+
+                for (let el of target) {
+                    removeClass("mb-animation", el);
+
+                    addClass("animation-show", el);
+                    let animation = document.getElementsByClassName("animation-show");
+                    for (let el of animation) {
+                        el.style.visibility = "visible";
+                    }
+                    removeClass(className, el);
+                }
+                fun();
+
+            }
+        });
+
+    };
+
+    let observer = new IntersectionObserver(callback, options);
+    observer.observe(document.querySelector(target));
+
+};
 
 
-// load
+
+
+//////////// enable and disable scrolling///////////
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+let keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+// modern Chrome requires { passive: false } when adding event
+let supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function() { supportsPassive = true; }
+    }));
+} catch (e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+
+const disableScroll = () => {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+};
+
+const enableScroll = () => {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+};
+
+
+/////////////////////////// mobile///////////////////////
+
+//toggle
+
+const toggleBtn = document.getElementById("toggle-btn");
+const menuSm = document.getElementById("mobile-menu");
+const menuSmItemList = document.getElementsByClassName("mb-menu-item");
+const playToggle = () => {
+    toggleBtn.addEventListener("load", e => {
+        let svgDoc = toggleBtn.contentDocument;
+        let svgIcon = svgDoc.getElementById("toggle-menu");
+        let line1 = svgDoc.getElementById("line-1");
+        let line2 = svgDoc.getElementById("line-2");
+        let line3 = svgDoc.getElementById("line-3");
+        let stroke1 = line2.getAttribute('stroke');
+        let stroke2 = "#52442a";
+        let active = false;
+        svgIcon.addEventListener("click", e => {
+
+            if (active) {
+                line3.animate([
+                    { transform: 'rotate(0deg)' }
+                ], {
+                    duration: 300,
+                    fill: 'forwards'
+                });
+                line2.animate([
+                    { transform: 'rotate(0deg)' }
+                ], {
+                    duration: 300,
+                    fill: 'forwards'
+                });
+
+                line3.animate([
+                    { transform: 'translateY(' + (line2.getAttribute('x1') / 2) + 'px)' }
+                ], {
+                    delay: 300,
+                    duration: 200,
+                    fill: 'forwards',
+                });
+                line1.animate([
+                    { transform: 'rotate(0deg)' }
+                ], {
+                    delay: 500,
+                    duration: 200,
+                    fill: 'forwards',
+                });
+                toggleBtn.animate([
+                    { transform: 'rotate(0deg)' }
+                ], {
+                    delay: 700,
+                    duration: 300,
+                    fill: 'forwards',
+                });
+                setTimeout(() => {
+                    line2.setAttribute('stroke', stroke1);
+                    line3.setAttribute('stroke', stroke1);
+                }, 490);
+                active = false;
+
+            } else {
+                toggleBtn.animate([
+                    { transform: 'rotate(-90deg)' }
+                ], {
+                    duration: 300,
+                    fill: 'forwards',
+                });
+                line1.animate([
+                    { transform: 'rotate(-90deg)' }
+                ], {
+                    delay: 300,
+                    duration: 200,
+                    fill: 'forwards',
+                });
+                line3.animate([
+                    { transform: 'translateY(' + (-line2.getAttribute('x1') / 2) + 'px)' }
+                ], {
+                    delay: 500,
+                    duration: 200,
+                    fill: 'forwards',
+                });
+                line3.setAttribute('y1', line2.getAttribute('y1'));
+                line3.setAttribute('y2', line2.getAttribute('y2'));
+                line2.style.transformOrigin = "center center";
+                line3.style.transformOrigin = "center center";
+
+                line2.animate([
+                    { transform: 'rotate(-45deg)' }
+                ], {
+                    delay: 700,
+                    duration: 200,
+                    fill: 'forwards'
+                });
+                line3.animate([
+                    { transform: 'rotate(45deg)' }
+                ], {
+                    delay: 700,
+                    duration: 200,
+                    fill: 'forwards'
+                });
+                setTimeout(() => {
+
+                    line2.setAttribute('stroke', stroke2);
+                    line3.setAttribute('stroke', stroke2);
+                }, 490);
+                active = true;
+            }
+            // menu components load
+            if (active) {
+                setTimeout(() => {
+                    menuSm.style.display = 'block';
+                    toggleBtn.style.position = "fixed";
+                    toggleBtn.style.left = "70%";
+                    disableScroll();
+                }, 500);
+
+
+            } else {
+                setTimeout(() => {
+                    menuSm.style.display = 'none';
+                    toggleBtn.style.position = 'initial';
+                    toggleBtn.style.left = "0";
+                    enableScroll();
+                }, 400);
+
+            }
+
+        });
+    });
+};
+
+
+const mobilePortfolio = () => {
+    if (innerWidth < 500) {
+        gsap.from(".port-3", { height: 0, borderTopLeftRadius: 200, borderBottomLeftRadius: 200, duration: 3 });
+        gsap.from(".port-4", { width: 0, borderTopRightRadius: 200, borderBottomRightRadius: 200, duration: 2 });
+    }
+};
+
+const mobilePortfolio_2 = () => {
+    if (innerWidth < 500) {
+        gsap.from(".port-6", { height: 0, width: 0, borderBottomRightRadius: 200, duration: 3 });
+    }
+
+};
+
+const mobilePortfolioText = () => {
+    if (innerWidth < 500) {
+        gsap.from(".port-text", { y: -30, duration: 2 });
+    }
+};
+
+
+
+// load all
 document.addEventListener('DOMContentLoaded', () => {
 
     ready();
@@ -129,15 +380,25 @@ document.addEventListener('DOMContentLoaded', () => {
         "#int-img-2",
         "#int-img-3");
 
+
     animate_lvl_2(".video-1", vid, "vid-container");
 
+    mobileAnimate_lvl_1(mobilePortfolio, 'inactive', ".port-3", ".port-4");
+    mobileAnimate_lvl_1(mobilePortfolio_2, 'inactive', ".port-6");
+    mobileAnimate_lvl_1(mobilePortfolioText, 'inactive', ".port-7", ".port-8",
+        ".port-9");
+
     videoPlay();
+
+    playToggle();
 
 });
 
 document.getElementById("portfolio").addEventListener("mouseover", e => {
     portfolio();
 });
+
+
 
 document.querySelector(".form-filling").addEventListener("mouseover", e => {
     contactForm();
